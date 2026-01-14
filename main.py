00a0ff3ed824502a18c776cb247a22da7021ebd7
theworkmerc <theworkmerc@gmail.com>
2026-01-13 17:28:12 -0700

    Added new constants to constants.py for asteroid data. Created new asteroid.py to create asteroid class. Created new asteroidfield.py that defines the behavior of asteroids. Updated main.py to call asteroids and asteroidfields to create instances in the main program.

--- main.py
+++ main.py
@@ -2,18 +2,29 @@ import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    dt = 0
+    
+
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    asteroid_field = AsteroidField()
+
     Player.containers = (updatable, drawable)
     player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
 
+    dt = 0
 
     while True:
         log_state()
